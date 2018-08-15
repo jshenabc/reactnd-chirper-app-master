4fc557bed5c782e2b3dc55e795d2efc8abada8be
shenjia <jshenabc@gmail.com>
2018-08-15 18:38:20 -0400

    New Tweet UI

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react'
 
 class NewTweet extends Component {
+  //when you type the submit button will change so we need to use this.state.
+  state= {
+    text: '',
+  }
 
+  handleChange = (e) => {
+    const text = e.target.value
+    this.setState(() => ({
+      text
+    }))
+  }
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const { text } = this.state
+    console.log('new tweet',text)
+    //todo: add tweet to stroe
+    this.setState(() => ({
+      text: ''
+    }))
+  }
   render() {
+    const { text } = this.state
+    const tweetLeft = 280 - text.length
     return (
       <div>
-        New Tweet
+         <h3 className='center'>Compose new Tweet</h3>
+          <form className='new-tweet' onSubmit={this.handleSubmit}>
+            <textarea
+              placeholder="What's happening?"
+              value={text}
+              onChange={this.handleChange}
+              className='textarea'
+              maxLength={280}
+            />
+            {tweetLeft <= 100 && (
+              <div className='tweet-length'>
+                {tweetLeft}
+              </div>
+            )}
+            <button
+              className='btn'
+              type='submit'
+              disabled={text === ''}>
+                Submit
+            </button>
+          </form>
       </div>
     )
   }
